64909ae5c98fa31f4117ab6ecd3b66f3d596c050
chunky87 <jeremynchan@gmail.com>
2024-10-27 21:26:51 -0700

    Basic main menu and character movement. Sprite cannot move out of bounds

---
+++ player.py
@@ -0,0 +1,16 @@
+import arcade
+import constants
+
+class Player(arcade.Sprite):
+    def update(self):
+        super().update()
+
+        if self.left < 0:
+            self.left = 0
+        elif self.right > constants.SCREEN_WIDTH - 1:
+            self.right = constants.SCREEN_WIDTH - 1
+
+        if self.bottom < 0:
+            self.bottom = 0
+        elif self.top > constants.SCREEN_HEIGHT - 1:
+            self.top = constants.SCREEN_HEIGHT - 1
